c955f271bfe3a3c677562cd62591d66e4beb85d6
Arsen Adamyan <arsenadamyan2494@gmail.com>
2018-05-20 16:24:48 +0400

    hashed password

--- api/models/user.js
+++ api/models/user.js
@@ -1,20 +1,38 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt');
 const userSchema = new Schema({
-    username: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true,
-        trim: true,
-        unique: 1
-    },
-    password: {
-        type: String,
-        required: true
-    },
+  username: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    unique: 1
+  },
+  password: {
+    type: String,
+    required: true
+  },
+});
+
+userSchema.pre('save', function (next) {
+  const user = this;
+
+  if (user.isModified('password')) {
+    bcrypt.genSalt(10, function (err, salt) {
+      if (err) return next(err);
+      bcrypt.hash(user.password, salt, function (err, hash) {
+        if (err) return next(err);
+        user.password = hash;
+        next();
+      });
+    });
+  } else {
+    next();
+  }
 });
 
 module.exports = mongoose.model('User', userSchema);
