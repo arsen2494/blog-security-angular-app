e9efae5f60517237399bef94a7481e6ff8c39d7f
Arsen Adamyan <arsenadamyan2494@gmail.com>
2018-05-21 18:17:19 +0400

    fixes

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -2,7 +2,7 @@ const User = require('../models/user');
 
 exports.createUser = function (req, res) {
   const user = new User(req.query);
-  user.save((err, save) => {
+  user.save((err, user) => {
     if (err) return res.status(400).send(err);
     res.status(201).json(user);
   });
